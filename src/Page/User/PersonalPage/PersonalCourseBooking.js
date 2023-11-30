8d6a919a21e84d8668bf6c022b0b17cf3d9a7237
ThaoHuynhD <138956970+ThaoHuynhD@users.noreply.github.com>
2023-11-30 09:13:33 +0700

    Update PersonalCourseBooking.js

--- src/Page/User/PersonalPage/PersonalCourseBooking.js
+++ src/Page/User/PersonalPage/PersonalCourseBooking.js
@@ -55,7 +55,7 @@ export default function PersonalCourseBooking({ userDetail }) {
             </div>
             <div>{renderUserRegisteredCourseList()}</div>
             <div className="text-right">
-                <p className='lg:text-2xl text-sm lg:p-10 py-4'>View Other Courses of Educator at <span className='cursor-pointer font-semibold underline text-purple-900'
+                <p className='lg:text-xl w-1/2 md:w-full text-sm lg:p-10 py-4'>View Other Courses of Educator at <span className='cursor-pointer font-semibold underline text-purple-900'
                     onClick={() => { navigate('/') }}>HERE</span></p>
             </div>
         </div>
