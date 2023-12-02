3c74bb471b13b081672040851016333be9247cca
DuyNguyenlk22 <nguyenduylk202@gmail.com>
2023-12-02 13:02:24 +0700

    fix

--- src/Page/Login/SignUpPage/SignUpPage.js
+++ src/Page/Login/SignUpPage/SignUpPage.js
@@ -18,7 +18,7 @@ export default function SignUpPage() {
       },
       lg: {
         span: 5,
-      }
+      },
     },
     wrapperCol: {
       xs: {
@@ -29,7 +29,7 @@ export default function SignUpPage() {
       },
       lg: {
         span: 24,
-      }
+      },
     },
   };
   const handleSubmit = (values) => {
@@ -37,10 +37,12 @@ export default function SignUpPage() {
     let fetchDataUserRegister = async () => {
       try {
         await dangKy(values);
-        message.success("Congratulations, you have assigned successfully, please sign in now");
+        message.success(
+          "Congratulations, you have assigned successfully, please sign in now",
+        );
         setTimeout(() => {
           window.location.reload();
-          window.location.href = "/signIn"
+          window.location.href = "/signIn";
         }, 1000);
       } catch (err) {
         message.error(err.response.data);
@@ -49,105 +51,121 @@ export default function SignUpPage() {
     fetchDataUserRegister();
   };
 
-  return <div id='signUp' className='w-screen h-screen relative ' style={{
-    backgroundImage: 'url(./image/bgLogin.png)',
-    backgroundSize: 'cover'
-  }}>
-    <div className='container border'>
-      <div className='text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 lg:w-1/2'>
-        <Form
-          className=' lg:p-20 p-5 border bg-purple-300 bg-opacity-60 rounded-2xl'
-          theme={'dark'}
-          {...formItemLayout}
-          form={form}
-          name="register"
-          onFinish={handleSubmit}
-          style={{
-            maxWidth: 800,
-          }}
-          scrollToFirstError
-        >
-          <h1 className='pb-5 text-4xl font-semibold'>SIGN UP</h1>
-          <Form.Item name="taiKhoan"
-            label="Account"
-            rules={[
-              {
-                required: true,
-                message: 'Please Insert your account!',
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item name="matKhau"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please Insert your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item name="soDienThoai"
-            label="Phone Number"
-            rules={[
-              {
-                required: true,
-                message: 'Please Insert your Phone Number!',
-                whitespace: true,
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="maNhom"
-            label="Group Code"
-            className='hidden'
-            initialValue={'GP09'}
-            rules={[
-              {
-                required: true,
-                message: 'Please Insert your group code(ex: GP09)!',
-                whitespace: true,
-              },
-            ]}>
-            <Input disabled={true} placeholder={'GP09'} />
-          </Form.Item>
-          <Form.Item name="hoTen"
-            label="Fullname"
-            rules={[
-              {
-                required: true,
-                message: 'Please Insert your fullname!',
-                whitespace: true,
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="email"
-            label="Email Address"
-            rules={[
-              {
-                type: 'email',
-                message: 'Your answer is not a valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please insert your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <button className='btnGlobal' htmlType="submit">
-            Sign up
-          </button>
-          <p className='pt-3'>If you already have an account, click <NavLink to='/signIn' className={'text-pink-600 hover:text-purple-800 font-bold duration-125'}>HERE</NavLink></p>
-        </Form>
+  return (
+    <div
+      id='signUp'
+      className='w-screen h-screen relative '
+      style={{
+        backgroundImage: "url(./image/bgLogin.png)",
+        backgroundSize: "cover",
+      }}>
+      <div className='container border'>
+        <div className='text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 lg:w-1/2'>
+          <Form
+            className=' lg:p-20 p-5 border bg-purple-300 bg-opacity-60 rounded-2xl'
+            theme={"dark"}
+            {...formItemLayout}
+            form={form}
+            name='register'
+            onFinish={handleSubmit}
+            style={{
+              maxWidth: 800,
+            }}
+            scrollToFirstError>
+            <h1 className='pb-5 text-4xl font-semibold'>SIGN UP</h1>
+            <Form.Item
+              name='taiKhoan'
+              label='Account'
+              rules={[
+                {
+                  required: true,
+                  message: "Please Insert your account!",
+                  whitespace: true,
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name='matKhau'
+              label='Password'
+              rules={[
+                {
+                  required: true,
+                  message: "Please Insert your password!",
+                },
+              ]}
+              hasFeedback>
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              name='soDienThoai'
+              label='Phone Number'
+              rules={[
+                {
+                  required: true,
+                  message: "Please Insert your Phone Number!",
+                  whitespace: true,
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name='maNhom'
+              label='Group Code'
+              className='hidden'
+              initialValue={"GP09"}
+              rules={[
+                {
+                  required: true,
+                  message: "Please Insert your group code(ex: GP09)!",
+                  whitespace: true,
+                },
+              ]}>
+              <Input disabled={true} placeholder={"GP09"} />
+            </Form.Item>
+            <Form.Item
+              name='hoTen'
+              label='Fullname'
+              rules={[
+                {
+                  required: true,
+                  message: "Please Insert your fullname!",
+                  whitespace: true,
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name='email'
+              label='Email Address'
+              rules={[
+                {
+                  type: "email",
+                  message: "Your answer is not a valid E-mail!",
+                },
+                {
+                  required: true,
+                  message: "Please insert your E-mail!",
+                },
+              ]}>
+              <Input />
+            </Form.Item>
+            <button className='btnGlobal' htmlType='submit'>
+              Sign up
+            </button>
+            <p className='pt-3'>
+              If you already have an account, click{" "}
+              <NavLink
+                to='/signIn'
+                className={
+                  "text-pink-600 hover:text-purple-800 font-bold duration-125"
+                }>
+                HERE
+              </NavLink>
+            </p>
+          </Form>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
--- src/Page/User/PersonalPage/PersonalPage.js
+++ src/Page/User/PersonalPage/PersonalPage.js
@@ -1,49 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { thongTinTaiKhoan } from "../../../Services/api";
-import { localServices, userDetailLocalStorage } from "../../../Services/localServices";
+import {
+  localServices,
+  userDetailLocalStorage,
+} from "../../../Services/localServices";
 import PersonalInfo from "./PersonalInfo";
 import PersonalCourseBooking from "./PersonalCourseBooking";
 import { useNavigate } from "react-router-dom";
 import { message } from "antd";
 export default function PersonalPage() {
-    let info = localServices.get();
-    const [userDetail, setUserDetail] = useState([]);
-    let navigate = useNavigate();
+  let info = localServices.get();
+  const [userDetail, setUserDetail] = useState([]);
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        const info = localServices.get();
-        if (info === null || info === undefined) {
-            message.error("You need to Sign In first to use this page");
-            navigate('/signIn');
-        }
-        const fetchDataUserDetail = async () => {
-            try {
-                const response = await thongTinTaiKhoan();
-                userDetailLocalStorage.set(response.data);
-                setUserDetail(userDetailLocalStorage.get());
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchDataUserDetail();
-    }, [navigate]);
-    return <div className="mt-20 container">
-        <div className="bg-white-900 text-purple-900 shadow-2xl text-center rounded-lg lg:flex lg:mt-20 mt-5">
-            <div className="lg:w-1/2 w-full lg:pl-10 pb-5">
-                <p className="lg:pt-24 pt-10 font-extrabold text-3xl ">Hello, {userDetail.hoTen}</p>
-                <p className="pt-5">Wellcome back to Educator!!!</p>
-            </div>
-            <div className="lg:w-1/2 w-full h-72 text-center relative overflow-hidden ">
-                <img className="w-96 mx-auto relative bottom-16" src="./image/personal/banner.svg" alt="null" />
-            </div>
+  useEffect(() => {
+    const info = localServices.get();
+    if (info === null || info === undefined) {
+      message.error("You need to Sign In first to use this page");
+      navigate("/signIn");
+    }
+    const fetchDataUserDetail = async () => {
+      try {
+        const response = await thongTinTaiKhoan();
+        userDetailLocalStorage.set(response.data);
+        setUserDetail(userDetailLocalStorage.get());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDataUserDetail();
+  }, [navigate]);
+  return (
+    <div className='mt-20 container'>
+      <div className='bg-white-900 text-purple-900 shadow-2xl text-center rounded-lg lg:flex lg:mt-20 mt-5'>
+        <div className='lg:w-1/2 w-full lg:pl-10 pb-5'>
+          <p className='lg:pt-24 pt-10 font-extrabold text-3xl '>
+            Hello, {userDetail.hoTen}
+          </p>
+          <p className='pt-5'>Wellcome back to Educator!!!</p>
         </div>
-        <div className="grid lg:grid-cols-7 grid-cols-1 my-5">
-            <div className="lg:col-span-2">
-                <PersonalInfo userDetail={userDetail} />
-            </div>
-            <div className="lg:col-span-5">
-                <PersonalCourseBooking userDetail={userDetail} />
-            </div>
+        <div className='lg:w-1/2 w-full h-72 text-center relative overflow-hidden '>
+          <img
+            className='w-96 mx-auto relative bottom-16'
+            src='./image/personal/banner.svg'
+            alt='null'
+          />
         </div>
-    </div>;
+      </div>
+      <div className='grid lg:grid-cols-7 grid-cols-1 my-5'>
+        <div className='lg:col-span-2'>
+          <PersonalInfo userDetail={userDetail} />
+        </div>
+        <div className='lg:col-span-5'>
+          <PersonalCourseBooking userDetail={userDetail} />
+        </div>
+      </div>
+    </div>
+  );
 }
